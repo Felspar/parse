63d9b56299c0d1298c7cb7b9f49f9247bb5e168e
Kirit Sælensminde <kirit@kirit.com>
2022-11-25 18:58:06 +0700

    Really this is the unchecked extract

--- include/felspar/parse/extract.hpp
+++ include/felspar/parse/extract.hpp
@@ -12,31 +12,43 @@ namespace felspar::parse::binary {
     /// Extract an instance of a requested type from the memory pointed to by
     /// the span
     template<typename T>
-    T extract(
+    T unchecked_extract(
             std::span<std::uint8_t> &,
             felspar::source_location const & =
                     felspar::source_location::current());
 
-    template<typename T, std::size_t N>
-    inline T
-            extract(std::span<std::byte, N> &s,
-                    felspar::source_location const &loc =
-                            felspar::source_location::current()) {
-        return extract(
-                std::span<std::uint8_t, N>{
-                        reinterpret_cast<std::uint8_t>(s.data()), s.size()},
-                loc);
+    template<typename T>
+    inline T unchecked_extract(
+            std::span<std::byte> &s,
+            felspar::source_location const &loc =
+                    felspar::source_location::current()) {
+        auto i = std::span<std::uint8_t>{
+                reinterpret_cast<std::uint8_t *>(s.data()), s.size()};
+        auto const v = unchecked_extract<T>(i, loc);
+        s = std::span<std::byte>{
+                reinterpret_cast<std::byte *>(i.data()), i.size()};
+        return v;
     }
 
-    template<typename T, std::size_t N>
-    inline T
-            extract(std::span<char, N> &s,
-                    felspar::source_location const &loc =
-                            felspar::source_location::current()) {
-        return extract(
-                std::span<std::uint8_t, N>{
-                        reinterpret_cast<std::uint8_t>(s.data()), s.size()},
-                loc);
+    // template<typename T>
+    // inline T
+    //         extract(std::span<char> &s,
+    //                 felspar::source_location const &loc =
+    //                         felspar::source_location::current()) {
+    //     return extract<T>(
+    //             std::span<std::uint8_t>{
+    //                     reinterpret_cast<std::uint8_t*>(s.data()), s.size()},
+    //             loc);
+    // }
+
+
+    /// Bytes
+    template<>
+    inline std::uint8_t unchecked_extract<std::uint8_t>(
+            std::span<std::uint8_t> &s, felspar::source_location const &) {
+        auto const v = s[0];
+        s = s.subspan(1);
+        return v;
     }
 
 
--- test/run/extract.tests.cpp
+++ test/run/extract.tests.cpp
@@ -8,7 +8,13 @@ namespace {
     auto const suite = felspar::testsuite("extract");
 
 
-    auto const i = suite.test([](auto check) {});
+    auto const i = suite.test([](auto check) {
+        std::array<std::uint8_t, 1> ar{123};
+        std::span<std::uint8_t> sp{ar};
+        check(felspar::parse::binary::unchecked_extract<std::uint8_t>(sp))
+                == 123;
+        check(sp.empty()) == true;
+    });
 
 
 }
