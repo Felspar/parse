e9016ff3834fcf1b45f75e74949934a997e558f6
Kirit Sælensminde <kirit@kirit.com>
2022-11-25 18:29:34 +0700

    Set up initial extract functions

--- include/felspar/parse/extract.hpp
+++ include/felspar/parse/extract.hpp
@@ -1,1 +1,43 @@
 #pragma once
+
+
+#include <span>
+
+#include <felspar/test/source.hpp>
+
+
+namespace felspar::parse::binary {
+
+
+    /// Extract an instance of a requested type from the memory pointed to by
+    /// the span
+    template<typename T>
+    T extract(
+            std::span<std::uint8_t> &,
+            felspar::source_location const & =
+                    felspar::source_location::current());
+
+    template<typename T, std::size_t N>
+    inline T
+            extract(std::span<std::byte, N> &s,
+                    felspar::source_location const &loc =
+                            felspar::source_location::current()) {
+        return extract(
+                std::span<std::uint8_t, N>{
+                        reinterpret_cast<std::uint8_t>(s.data()), s.size()},
+                loc);
+    }
+
+    template<typename T, std::size_t N>
+    inline T
+            extract(std::span<char, N> &s,
+                    felspar::source_location const &loc =
+                            felspar::source_location::current()) {
+        return extract(
+                std::span<std::uint8_t, N>{
+                        reinterpret_cast<std::uint8_t>(s.data()), s.size()},
+                loc);
+    }
+
+
+}
---
+++ test/run/extract.tests.cpp
@@ -0,0 +1,14 @@
+#include <felspar/parse/extract.hpp>
+#include <felspar/test.hpp>
+
+
+namespace {
+
+
+    auto const suite = felspar::testsuite("extract");
+
+
+    auto const i = suite.test([](auto check) {});
+
+
+}
