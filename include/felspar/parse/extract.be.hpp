56d297bcce3fd77b45ca4c2e889be17440127595
Kirit Sælensminde <kirit@felspar.com>
2023-01-15 17:07:08 +0700

    We only extract from const buffers

--- include/felspar/parse/extract.be.hpp
+++ include/felspar/parse/extract.be.hpp
@@ -12,32 +12,32 @@ namespace felspar::parse::binary::be {
     /// Extract an instance of a requested type from the memory pointed to by
     /// the span
     template<typename T>
-    T unchecked_extract(std::span<std::uint8_t, sizeof(T)>) noexcept;
+    T unchecked_extract(std::span<std::uint8_t const, sizeof(T)>) noexcept;
 
     template<typename T>
-    inline T unchecked_extract(std::span<std::byte, sizeof(T)> s) noexcept {
-        return unchecked_extract<T>(std::span<std::uint8_t, sizeof(T)>{
-                reinterpret_cast<std::uint8_t *>(s.data()), s.size()});
+    inline T unchecked_extract(std::span<std::byte const, sizeof(T)> s) noexcept {
+        return unchecked_extract<T>(std::span<std::uint8_t const, sizeof(T)>{
+                reinterpret_cast<std::uint8_t const *>(s.data()), s.size()});
     }
 
     template<typename T>
-    inline T unchecked_extract(std::span<char, sizeof(T)> s) noexcept {
-        return unchecked_extract<T>(std::span<std::uint8_t, sizeof(T)>{
-                reinterpret_cast<std::uint8_t *>(s.data()), s.size()});
+    inline T unchecked_extract(std::span<char const, sizeof(T)> s) noexcept {
+        return unchecked_extract<T>(std::span<std::uint8_t const, sizeof(T)>{
+                reinterpret_cast<std::uint8_t const *>(s.data()), s.size()});
     }
 
 
     /// Bytes
     template<>
     inline std::uint8_t unchecked_extract<std::uint8_t>(
-            std::span<std::uint8_t, 1> s) noexcept {
+            std::span<std::uint8_t const, 1> s) noexcept {
         return s[0];
     }
 
     /// 16 bit/WORD
     template<>
     inline std::uint16_t unchecked_extract<std::uint16_t>(
-            std::span<std::uint8_t, 2> s) noexcept {
+            std::span<std::uint8_t const, 2> s) noexcept {
         return (s[0] << 8) + s[1];
     }
 
@@ -45,12 +45,13 @@ namespace felspar::parse::binary::be {
     /// Extract from a variable length data span
     template<typename T>
     inline T
-            extract(std::span<std::uint8_t> &s,
+            extract(std::span<std::uint8_t const> &s,
                     felspar::source_location const &loc =
                             felspar::source_location::current()) {
         if (s.size() >= sizeof(T)) {
             auto const v = unchecked_extract<T>(
-                    std::span<T, sizeof(T)>{s.data(), sizeof(T)});
+                    std::span<std::uint8_t const, sizeof(T)>{
+                            s.data(), sizeof(T)});
             s = s.subspan(sizeof(T));
             return v;
         } else {
