043a8584fb9961a78aab4fdad1f5fdaab8f3a670
Kirit Sælensminde <kirit@kirit.com>
2023-01-19 19:38:57 +0700

    Add extract API for little endian values

---
+++ include/felspar/parse/extract.le.hpp
@@ -0,0 +1,82 @@
+
+
+#include <felspar/exceptions.hpp>
+
+#include <span>
+
+
+namespace felspar::parse::binary::le {
+
+
+    /// Extract an instance of a requested type from the memory pointed to by
+    /// the span
+    template<typename T>
+    T unchecked_extract(std::span<std::uint8_t const, sizeof(T)>) noexcept;
+
+    template<typename T>
+    inline T unchecked_extract(
+            std::span<std::byte const, sizeof(T)> const s) noexcept {
+        return unchecked_extract<T>(std::span<std::uint8_t const, sizeof(T)>{
+                reinterpret_cast<std::uint8_t const *>(s.data()), s.size()});
+    }
+
+    template<typename T>
+    inline T unchecked_extract(
+            std::span<char const, sizeof(T)> const s) noexcept {
+        return unchecked_extract<T>(std::span<std::uint8_t const, sizeof(T)>{
+                reinterpret_cast<std::uint8_t const *>(s.data()), s.size()});
+    }
+
+
+    /// Bytes
+    template<>
+    inline std::uint8_t unchecked_extract<std::uint8_t>(
+            std::span<std::uint8_t const, 1> const s) noexcept {
+        return s[0];
+    }
+
+    /// 16 bit/WORD
+    template<>
+    inline std::uint16_t unchecked_extract<std::uint16_t>(
+            std::span<std::uint8_t const, 2> const s) noexcept {
+        return (s[1] << 8) + s[0];
+    }
+
+    /// 32 bit/DWORD
+    template<>
+    inline std::uint32_t unchecked_extract<std::uint32_t>(
+            std::span<std::uint8_t const, 4> const s) noexcept {
+        return (s[3] << 24) + (s[2] << 16) + (s[1] << 8) + s[0];
+    }
+
+    /// 64 bit/QWORD
+    template<>
+    inline std::uint64_t unchecked_extract<std::uint64_t>(
+            std::span<std::uint8_t const, 8> const s) noexcept {
+        return (std::int64_t(s[7]) << 56) + (std::int64_t(s[6]) << 48)
+                + (std::int64_t(s[5]) << 40) + (std::int64_t(s[4]) << 32)
+                + (std::int64_t(s[3]) << 24) + (std::int64_t(s[2]) << 16)
+                + (std::int64_t(s[1]) << 8) + std::int64_t(s[0]);
+    }
+
+
+    /// Extract from a variable length data span
+    template<typename T>
+    inline T
+            extract(std::span<std::uint8_t const> &s,
+                    felspar::source_location const &loc =
+                            felspar::source_location::current()) {
+        if (s.size() >= sizeof(T)) {
+            auto const v = unchecked_extract<T>(
+                    std::span<std::uint8_t const, sizeof(T)>{
+                            s.data(), sizeof(T)});
+            s = s.subspan(sizeof(T));
+            return v;
+        } else {
+            throw felspar::stdexcept::logic_error{
+                    "Buffer doesn't have enough data", loc};
+        }
+    }
+
+
+}
--- test/run/extract.tests.cpp
+++ test/run/extract.tests.cpp
@@ -14,39 +14,77 @@ namespace {
         std::array<std::uint8_t, 1> ar{123};
         check(felspar::parse::binary::unchecked_extract<std::uint8_t>(ar))
                 == 123;
+        check(felspar::parse::binary::be::unchecked_extract<std::uint8_t>(ar))
+                == 123;
+        check(felspar::parse::binary::le::unchecked_extract<std::uint8_t>(ar))
+                == 123;
 
         std::array<std::byte, 1> ab{std::byte{124}};
         check(felspar::parse::binary::unchecked_extract<std::uint8_t>(ab))
                 == 124;
+        check(felspar::parse::binary::be::unchecked_extract<std::uint8_t>(ab))
+                == 124;
+        check(felspar::parse::binary::le::unchecked_extract<std::uint8_t>(ab))
+                == 124;
 
         std::array<char, 1> ac{125};
         check(felspar::parse::binary::unchecked_extract<std::uint8_t>(ac))
+                == 125;
+        check(felspar::parse::binary::be::unchecked_extract<std::uint8_t>(ac))
+                == 125;
+        check(felspar::parse::binary::le::unchecked_extract<std::uint8_t>(ac))
                 == 125;
     });
     auto const i16 = suite.test([](auto check) {
         std::array<std::uint8_t, 2> ar{0x12, 0x34};
         check(felspar::parse::binary::unchecked_extract<std::uint16_t>(ar))
                 == 0x1234;
+        check(felspar::parse::binary::be::unchecked_extract<std::uint16_t>(ar))
+                == 0x1234;
+        check(felspar::parse::binary::le::unchecked_extract<std::uint16_t>(ar))
+                == 0x3412;
     });
     auto const i32 = suite.test([](auto check) {
         std::array<std::uint8_t, 4> ar{0x12, 0x34, 0x56, 0x78};
         check(felspar::parse::binary::unchecked_extract<std::uint32_t>(ar))
                 == 0x1234'5678;
+        check(felspar::parse::binary::be::unchecked_extract<std::uint32_t>(ar))
+                == 0x1234'5678;
+        check(felspar::parse::binary::le::unchecked_extract<std::uint32_t>(ar))
+                == 0x7856'3412;
     });
     auto const i64 = suite.test([](auto check) {
         std::array<std::uint8_t, 8> ar{0x12, 0x34, 0x56, 0x78,
                                        0x98, 0x76, 0x54, 0x32};
         check(felspar::parse::binary::unchecked_extract<std::uint64_t>(ar))
                 == 0x1234'5678'9876'5432;
+        check(felspar::parse::binary::be::unchecked_extract<std::uint64_t>(ar))
+                == 0x1234'5678'9876'5432;
+        check(felspar::parse::binary::le::unchecked_extract<std::uint64_t>(ar))
+                == 0x3254'7698'7856'3412;
     });
 
 
     auto const mi8 = suite.test([](auto check) {
         std::vector<std::uint8_t> ar{95, 96};
-        std::span<std::uint8_t const> sp{ar.data(), ar.size()};
-        check(felspar::parse::binary::extract<std::uint8_t>(sp)) == 95;
-        check(felspar::parse::binary::extract<std::uint8_t>(sp)) == 96;
-        check(sp.empty()) == true;
+        {
+            std::span<std::uint8_t const> sp{ar.data(), ar.size()};
+            check(felspar::parse::binary::extract<std::uint8_t>(sp)) == 95;
+            check(felspar::parse::binary::extract<std::uint8_t>(sp)) == 96;
+            check(sp.empty()) == true;
+        }
+        {
+            std::span<std::uint8_t const> sp{ar.data(), ar.size()};
+            check(felspar::parse::binary::be::extract<std::uint8_t>(sp)) == 95;
+            check(felspar::parse::binary::be::extract<std::uint8_t>(sp)) == 96;
+            check(sp.empty()) == true;
+        }
+        {
+            std::span<std::uint8_t const> sp{ar.data(), ar.size()};
+            check(felspar::parse::binary::le::extract<std::uint8_t>(sp)) == 95;
+            check(felspar::parse::binary::le::extract<std::uint8_t>(sp)) == 96;
+            check(sp.empty()) == true;
+        }
     });
 
 
